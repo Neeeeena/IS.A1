bc4c440a69bc88c6db6297bd29ec80e2292a9b8e
Neeeeena <Nina@Ninas-MacBook-Pro.local>
2014-09-10 16:18:35 +0200

    Kommer ikke nogen vegne..

--- Assignment1/src/main.c
+++ Assignment1/src/main.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 #include "sensor.h"
 
+int sensorData[12];
+int lowpassData[32];
+
+
 int main() {
-	int i0;
 
 
-	i0 = getNextData();
-	printf("%d\n", i0);
-	i0 = getNextData();
-	printf("%d\n", i0);
+	for(int i = 0; i < 12; i++){
+		sensorData[i] = getNextData();
+	}
+
 
 	return 0;
 
+}
+
+int lowPass(){
+	int counter = 0;
+
+	int sensorData0 = sensorData [(counter)%12];
+	int sensorData5 = sensorData [(counter-5)%12];
+	int sensorData11 = sensorData [(counter-11)%12];
+
+	int lowpass0 = lowpassData [(counter)%32];
+	int lowpass1 = lowpassData [(counter - 1)%32];
+
+	return 2*lowpass0-lowpass1+1/32*(sensorData0-2*sensorData5+sensorData11);
+
+	//temp = 2*lowpassData[0]-info[1]+1/32*(sensorData[0]-2*sensorData[5]+sensorData[11]);
+
+	counter++;
 }
